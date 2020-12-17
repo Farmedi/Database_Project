db081f1bd90cdb29ead9962cb1994bc8cb64a844
FTOGH <53789439+FTOGH@users.noreply.github.com>
2020-12-17 21:25:23 +0300

    Öğretmen logini eklendi.

--- VeritabanıProjesi/Designs/Giris_Yonlendirme.cs
+++ VeritabanıProjesi/Designs/Giris_Yonlendirme.cs
@@ -26,14 +26,39 @@ namespace VeritabanıProjesi
 
         private void ogretmen_login_Click(object sender, EventArgs e)
         {
-            ogretmen_home og = new ogretmen_home();
-            og.Show();
-            this.Hide();
+            lbl_login_error.Text = "";
+            string email = tb_email.Text.Trim();
+            string password = tb_pw.Text.Trim();
+
+            SqlDataReader dr;
+            String str = "server = LAPTOP-1P0U4F0G; database=veritabanı_projesi;User Id=ftft;password=Hhft.1811asd159159159";
+            SqlConnection con = new SqlConnection(str);
+            string query = "select * from tbl_ogretmen where email='" + email + "' AND pw='" + password + "'";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            con.Open();
+
+            dr = cmd.ExecuteReader();
+            if (!dr.Read())
+            {
+                lbl_login_error.Text = "Please check your credentials.";
+                con.Close();
+            }
+            else
+            {
+                con.Close();
+                ogretmen_home ogretmen = new ogretmen_home();
+                ogretmen.Show();
+                this.Hide();
+
+            }
+
+
         }
 
         private void Ogrenci_login_Click(object sender, EventArgs e)
         {
-            lbl_login_error.Text = "";
+            lbl_login_error.Text = ""; 
             string email = tb_email.Text.Trim();
             string password = tb_pw.Text.Trim();
 
