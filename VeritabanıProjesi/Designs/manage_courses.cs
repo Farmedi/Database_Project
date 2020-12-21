d9eafb10883f247fd3200c06a38e324ded3e2fd5
FTOGH <53789439+FTOGH@users.noreply.github.com>
2020-12-21 15:35:27 +0300

    a

--- VeritabanıProjesi/Designs/manage_courses.cs
+++ VeritabanıProjesi/Designs/manage_courses.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace VeritabanıProjesi
 {
@@ -17,6 +18,15 @@ namespace VeritabanıProjesi
 
         private void manage_courses_Load(object sender, EventArgs e)
         {
+            String str = "server = LAPTOP-1P0U4F0G; database=veritabanı_projesi;User Id=ftft;password=Hhft.1811asd159159159";
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("list_courses_given", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
 
         }
 
